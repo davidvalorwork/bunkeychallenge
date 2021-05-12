f37a90c88b2e567c09c11e6d8e526a54c8939433
davidvalorwork <davidvalorwork@gmail.com>
2021-05-12 15:40:42 -0400

    modulos instalados con 'npm i express cors --save'

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+// Importando configuraciones globales
+const config  = require("./config.json")[process.argvs[2]]
+
 // Importando modulos 
 const express = require("express")
 const cors    = require("cors")
@@ -9,3 +12,8 @@ const app = express()
 app.use(cors({origin:"*"}))
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
+
+// Inicializando rutas
+
+// Inicializando Servidor
+app.listen(config.PORT,() => console.log(`App listening at port ${config.PORT}`))
